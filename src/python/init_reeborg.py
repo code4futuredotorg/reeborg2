2d64486325bdf0102d4d414cf3cc97fee9375846
coursemdetw <course@mde.tw>
2016-03-20 17:46:14 +0800

    確認在本地端必須以 python3 -m http.server 啟動

--- src/python/init_reeborg.py
+++ src/python/init_reeborg.py
@@ -1,6 +1,10 @@
 from browser import window
 from _importlib import optimize_import_for_path
-optimize_import_for_path('src/python', 'py')
+optimize_import_for_path('/src/python', 'py')
 from common import generic_translate_python
 window['translate_python'] = generic_translate_python
 import py_repl
+
+
+
+
